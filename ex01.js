775dd8e98808c410a71cd461ec6b94fabbd0b90b
Gabrielli <gabriellicorrea7@gmail.com>
2019-03-27 18:29:46 +0000

    Exercicio 1

---
+++ ex01.js
@@ -0,0 +1,13 @@
+function retornaNum(str){
+    var palavra = "amizade";
+    for(var i = 0; i<= palavra.length; i++){
+        var cont = i;
+        
+    }
+    return cont;
+}
+
+alert("A quantidade de letras eh" + retornaNum());
+
+
+
